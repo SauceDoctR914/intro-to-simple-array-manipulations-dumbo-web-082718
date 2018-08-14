6dc08ac3aaac76f60d404e18d93bd712c57bb041
Gavin Soko <sokolofg@gmail.com>
2018-08-14 02:23:19 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,10 +1,16 @@
 def using_push(array, string)
+  array.push(string)
+  array
 end
 
 def using_unshift(array, string)
+  array.unshift(string)
+  array
 end
 
 def using_pop(array)
+  array.pop
+  array
 end
 
 def pop_with_args(array)
