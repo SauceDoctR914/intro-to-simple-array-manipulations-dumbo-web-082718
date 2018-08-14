d8ad1da7ce5dfe76f6cf99850a7ed89ede215fe1
Gavin Soko <sokolofg@gmail.com>
2018-08-14 02:31:06 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -9,17 +9,14 @@ end
 
 def using_pop(array)
   array.pop
-  array
 end
 
 def pop_with_args(array)
   array.pop(2)
-  array
 end
 
-def using_shift
+def using_shift(array)
   array.shift
-  array
 end
 
 def shift_with_args(array, string)
