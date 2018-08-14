292c963bd544838f726fac3523c40870e031e917
Gavin Soko <sokolofg@gmail.com>
2018-08-14 02:35:25 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -11,7 +11,7 @@ def using_pop(array)
   array.pop
 end
 
-def pop_with_args(array)
+def pop_with_args(array, num)
   array.pop(2)
 end
 
@@ -24,21 +24,27 @@ def shift_with_args(array, string)
   array
 end
 
-def using_concat
+def using_concat(array1, array2)
+  array1.concat(array2)
 end
 
-def using_insert
+def using_insert(array, element)
+  array.insert(element)
 end
 
 def using_uniq(array)
+  array.uniq
 end
 
 def using_flatten(array)
+  array.flatten
 end
 
 def using_delete(array, string)
+  array.delete(string)
 end
 
 def using_delete_at(array, integer)
+  array.delete_
 end
   
